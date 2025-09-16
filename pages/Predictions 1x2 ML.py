8375a34815eec469a145634f1a7f47f14c165038
wcesarpsv <wcesarps@gmail.com>
2025-09-16 08:13:23 -0400

    Update Predictions 1x2 ML.py

--- pages/Predictions 1x2 ML.py
+++ pages/Predictions 1x2 ML.py
@@ -12,7 +12,7 @@ st.title("📊 AI-Powered Bet Indicator – Random Forest")
 # ---------------- Configs ----------------
 GAMES_FOLDER = "GamesDay"
 MODELS_FOLDER = "Models"
-EXCLUDED_LEAGUE_KEYWORDS = ["cup", "copas", "uefa"]
+EXCLUDED_LEAGUE_KEYWORDS = ["cup", "copas", "uefa","AFC"]
 
 os.makedirs(MODELS_FOLDER, exist_ok=True)
 
