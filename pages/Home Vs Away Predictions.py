0a0bdcb04fb3b1b7e5bcc5bebf18c358119cbb21
wcesarpsv <wcesarps@gmail.com>
2025-09-16 08:15:36 -0400

    Update Home Vs Away Predictions.py

--- pages/Home Vs Away Predictions.py
+++ pages/Home Vs Away Predictions.py
@@ -11,7 +11,7 @@ st.title("📊 AI-Powered Bet Indicator – Home vs Away (Binary)")
 # ---------------- Configs ----------------
 GAMES_FOLDER = "GamesDay"
 MODELS_FOLDER = "Models"
-EXCLUDED_LEAGUE_KEYWORDS = ["cup", "copas", "uefa","AFC Champions League Elite"]
+EXCLUDED_LEAGUE_KEYWORDS = ["cup", "copas", "uefa","afc"]
 
 os.makedirs(MODELS_FOLDER, exist_ok=True)
 
