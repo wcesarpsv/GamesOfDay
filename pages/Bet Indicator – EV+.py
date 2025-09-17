882783813abfbbd3b856337f152bc9e107b8d7e5
wcesarpsv <wcesarps@gmail.com>
2025-09-17 08:21:41 -0400

    Update Bet Indicator – EV+.py

--- pages/Bet Indicator – EV+.py
+++ pages/Bet Indicator – EV+.py
@@ -12,7 +12,7 @@ st.title("📊 AI-Powered Bet Indicator – Random Forest + Kelly")
 # ---------------- Configs ----------------
 GAMES_FOLDER = "GamesDay"
 MODELS_FOLDER = "Models"
-EXCLUDED_LEAGUE_KEYWORDS = ["cup", "copas", "uefa","afc","sudamericana"]
+EXCLUDED_LEAGUE_KEYWORDS = ["cup", "copas", "uefa","afc","sudamericana","copa"]
 
 os.makedirs(MODELS_FOLDER, exist_ok=True)
 
