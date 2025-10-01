55bf52f3827aecd9297c6a8a9c241a1980063ef5
wcesarpsv <wcesarps@gmail.com>
2025-10-01 10:34:29 -0400

    Update Thermometer + ML V1.py

--- pages/Thermometer + ML V1.py
+++ pages/Thermometer + ML V1.py
@@ -816,9 +816,9 @@ st.subheader("📊 Games – Rules vs ML (Fixed vs Kelly Staking)")
 st.dataframe(
     games_today[available_cols]
     # .style.format({
-    #     'Goals_H_Today': '{:.0f}',
-    #     'Goals_A_Today': '{:.0f}',
-        'Home_Red': '{:.0f}'
+        # 'Goals_H_Today': '{:.0f}',
+        # 'Goals_A_Today': '{:.0f}',
+        'Home_Red': '{:.0f}',
         'Away_Red': '{:.0f}',
         'Profit_Auto_Fixed': '{:.2f}',
         'Profit_Auto_Kelly': '{:.2f}',
