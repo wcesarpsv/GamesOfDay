24d8bb3d77b03c795f1bced6a65433d75eadbf65
wcesarpsv <wcesarps@gmail.com>
2025-10-01 08:46:33 -0400

    Update Bet Indicator - Correct Score.py

--- pages/Bet Indicator - Correct Score.py
+++ pages/Bet Indicator - Correct Score.py
@@ -16,7 +16,7 @@ st.title("📊 Bet Indicator – Correct Score (0x0 - 3x3)")
 
 # Paths
 GAMES_FOLDER = "GamesDay"
-EXCLUDED_LEAGUE_KEYWORDS = ["cup", "copa", "copas", "uefa", "nordeste", "afc"]
+EXCLUDED_LEAGUE_KEYWORDS = ["cup", "copa", "copas", "uefa", "nordeste", "afc","trophy"]
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODELS_FOLDER = os.path.join(BASE_DIR, "Models")
