74f6ff411423e01e0d4b3744ef9e9b76ceb9554e
wcesarpsv <wcesarps@gmail.com>
2025-10-07 16:14:48 -0400

    Update Handicap V2.py

--- pages/Handicap V2.py
+++ pages/Handicap V2.py
@@ -347,7 +347,7 @@ normalize_features = st.sidebar.checkbox("Normalize features (odds + strength + 
 def train_and_evaluate(X, y, name):
     safe_name = name.replace(" ", "")
     safe_model = ml_model_choice.replace(" ", "")
-    filename = f"{PAGE_PREFIX}_{safe_model}_{safe_name}_2C_v1.pkl"
+    filename = f"{PAGE_PREFIX}_{safe_model}_{safe_name}_2CH_v2.pkl"
     feature_cols = X.columns.tolist()
 
     if not retrain:
@@ -388,7 +388,7 @@ def train_and_evaluate(X, y, name):
 def train_and_evaluate_v2(X, y, name, use_calibration=True):
     safe_name = name.replace(" ", "")
     safe_model = ml_model_choice.replace(" ", "")
-    filename = f"{PAGE_PREFIX}_{safe_model}_{safe_name}_2C_v2.pkl"
+    filename = f"{PAGE_PREFIX}_{safe_model}_{safe_name}_2CH_v2.pkl"
     feature_cols = X.columns.tolist()
 
     if not retrain:
