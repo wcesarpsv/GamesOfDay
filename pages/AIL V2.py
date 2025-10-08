a2414f8ad168108f19fe634f0a4c38346bd49e34
wcesarpsv <wcesarps@gmail.com>
2025-10-08 12:58:50 -0400

    Update AIL V2.py

--- pages/AIL V2.py
+++ pages/AIL V2.py
@@ -236,7 +236,7 @@ def brier_multiclass(y_true, probs, classes):
 def train_and_evaluate(X, y, name):
     safe_name = name.replace(" ", "")
     safe_model = ml_model_choice.replace(" ", "")
-    filename = f"{PAGE_PREFIX}_{safe_model}_{safe_name}_2CH_v4.pkl"
+    filename = f"{PAGE_PREFIX}_{safe_model}_{safe_name}_2CH_v5.pkl"
     feature_cols = X.columns.tolist()
 
     if not retrain:
