688de0619674c2220240996e7394fd330ad4de0d
wcesarpsv <wcesarps@gmail.com>
2025-10-08 18:03:52 -0400

    Update AIL V2.py

--- pages/AIL V2.py
+++ pages/AIL V2.py
@@ -568,7 +568,7 @@ normalize_features = st.sidebar.checkbox("Normalize features", value=True)
 def train_and_evaluate(X, y, name):
     safe_name = name.replace(" ", "")
     safe_model = ml_model_choice.replace(" ", "")
-    filename = f"{PAGE_PREFIX}_{safe_model}_{safe_name}_2CH_v4.pkl"
+    filename = f"{PAGE_PREFIX}_{safe_model}_{safe_name}_2CH_v5.pkl"
     feature_cols = X.columns.tolist()
 
     if not retrain:
@@ -613,7 +613,7 @@ def train_and_evaluate(X, y, name):
 def train_and_evaluate_v2(X, y, name, use_calibration=True):
     safe_name = name.replace(" ", "")
     safe_model = ml_model_choice.replace(" ", "")
-    filename = f"{PAGE_PREFIX}_{safe_model}_{safe_name}_2CH_v4.pkl"
+    filename = f"{PAGE_PREFIX}_{safe_model}_{safe_name}_2CH_v5.pkl"
     feature_cols = X.columns.tolist()
 
     if not retrain:
