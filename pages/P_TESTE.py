3fd501834893eb262e25fc2ba5fe24f5d2699cb9
wcesarpsv <wcesarps@gmail.com>
2025-10-17 13:09:01 -0400

    Update P_TESTE.py

--- pages/P_TESTE.py
+++ pages/P_TESTE.py
@@ -927,7 +927,7 @@ if 'Market_Error_Home' not in finished_games.columns:
         # st.success("✅ Colunas Market_Error criadas com sucesso!")
     else:
         # st.error("❌ Odds não disponíveis - impossível calcular Market Error")
-        # st.stop()
+        st.stop()
 
 # AGORA SIM PODEMOS EXECUTAR A ANÁLISE
 def analyze_market_error_performance(df, side):
