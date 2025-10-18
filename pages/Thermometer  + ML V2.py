33dd7ab51997b7cfc0cf9df10b1aa631b7fe81d6
wcesarpsv <wcesarps@gmail.com>
2025-10-18 09:02:50 -0400

    Update Thermometer  + ML V2.py

--- pages/Thermometer  + ML V2.py
+++ pages/Thermometer  + ML V2.py
@@ -474,7 +474,7 @@ def _from_proba(
     if diff_gap > 0.05:
         # ---- Context layer ----
         ok_context = (
-            (m_a is not None and m_a > 0.5) and
+            (m_a is not None and m_a > 0.8) and
             (m_h is not None and m_h < 0.2) and
             (diff_m is not None and diff_m < -0.8) and
             (diff_power is not None and diff_power > -20)
