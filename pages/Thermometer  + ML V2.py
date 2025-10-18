ff977e4cabf1c8b4a3a2cbc64b23cfd6b2e15620
wcesarpsv <wcesarps@gmail.com>
2025-10-18 08:57:57 -0400

    Update Thermometer  + ML V2.py

--- pages/Thermometer  + ML V2.py
+++ pages/Thermometer  + ML V2.py
@@ -477,7 +477,7 @@ def _from_proba(
             (m_a is not None and m_a > 0.5) and
             (m_h is not None and m_h < 0.2) and
             (diff_m is not None and diff_m < -0.8) and
-            (diff_power is not None and diff_power > -15)
+            (diff_power is not None and diff_power > -20)
         )
 
         # Liga mais exigente
