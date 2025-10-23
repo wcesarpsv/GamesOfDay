4a17e98446b7592a0e5c37307852a0eb935e8a94
wcesarpsv <wcesarps@gmail.com>
2025-10-23 10:48:44 -0400

    Update SixteenQuadrants.py

--- pages/SixteenQuadrants.py
+++ pages/SixteenQuadrants.py
@@ -377,10 +377,10 @@ def plot_quadrantes_16(df, side="Home"):
     
     # 🎨 Cores nomeadas (tons claros = neutro / escuros = extremos)
     cores_quadrantes_16 = {
-        1: 'darkblue',  2: 'blue',  3: 'blue',       4: 'darkblue',     # Fav Forte
-        5: 'green', 6: 'black', 7: 'black',     8: 'green',    # Fav Moderado
-        9: 'black',  10: 'black',        11: 'black',    12: 'black',   # Under Moderado
-        13: 'indianred',14: 'indianred',  15: 'darkred',       16: 'darkred'      # Under Forte
+        1: 'blue',  2: 'black',  3: 'black', 4: 'red',     # Fav Forte
+        5: 'darkblue', 6: 'black', 7: 'black', 8: 'darkred',    # Fav Moderado
+        9: 'darkblue',  10: 'black', 11: 'black', 12: 'darkred',   # Under Moderado
+        13: 'blue',14: 'black',  15: 'black', 16: 'red'  # Under Forte
     }
 
     # 🔹 Plotar cada ponto de acordo com o quadrante
