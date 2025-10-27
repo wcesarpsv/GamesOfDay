10195a68d81da549e2282785382a248c115f7752
wcesarpsv <wcesarps@gmail.com>
2025-10-27 12:20:22 -0400

    Update HandNew.py

--- pages/HandNew.py
+++ pages/HandNew.py
@@ -367,8 +367,8 @@ def calcular_distancias_quadrantes(df):
         dy = df['HandScore_Home'] - df['HandScore_Away']
         df['Quadrant_Dist'] = np.sqrt(dx**2 + (dy/60)**2 * 2.5) * 10  # escala visual ajustada
         df['Quadrant_Separation'] = 0.5 * (dy + 60 * dx)
-        # df['Quadrant_Angle'] = np.degrees(np.arctan2(dy, dx))
-        df['Quadrant_Angle'] = np.degrees(np.arctan2((dy / 60), dx))
+        df['Quadrant_Angle_Geometric'] = np.degrees(np.arctan2(dy, dx))
+        df['Quadrant_Angle_Normalized'] = np.degrees(np.arctan2((dy / 60), dx))
     else:
         st.warning("⚠️ Colunas Aggression/HandScore não encontradas para calcular as distâncias.")
         df['Quadrant_Dist'] = np.nan
@@ -745,7 +745,7 @@ def treinar_modelo_quadrantes_dual(history, games_today):
     ligas_dummies = pd.get_dummies(history['League'], prefix='League')
 
     # 🔹 Novas features contínuas (Distância, Separação e Ângulo)
-    extras = history[['Quadrant_Dist', 'Quadrant_Separation', 'Quadrant_Angle']].fillna(0)
+    extras = history[['Quadrant_Dist', 'Quadrant_Separation', 'Quadrant_Angle_Geometric', 'Quadrant_Angle_Normalized']].fillna(0)
 
     # Combinar todas as features
     X = pd.concat([quadrantes_home, quadrantes_away, ligas_dummies, extras], axis=1)
@@ -773,7 +773,10 @@ def treinar_modelo_quadrantes_dual(history, games_today):
     qh_today = pd.get_dummies(games_today['Quadrante_Home'], prefix='QH').reindex(columns=quadrantes_home.columns, fill_value=0)
     qa_today = pd.get_dummies(games_today['Quadrante_Away'], prefix='QA').reindex(columns=quadrantes_away.columns, fill_value=0)
     ligas_today = pd.get_dummies(games_today['League'], prefix='League').reindex(columns=ligas_dummies.columns, fill_value=0)
-    extras_today = games_today[['Quadrant_Dist', 'Quadrant_Separation', 'Quadrant_Angle']].fillna(0)
+    extras_today = games_today[['Quadrant_Dist', 
+                            'Quadrant_Separation', 
+                            'Quadrant_Angle_Geometric', 
+                            'Quadrant_Angle_Normalized']].fillna(0)
 
     X_today = pd.concat([qh_today, qa_today, ligas_today, extras_today], axis=1)
 
