d63fc26f45356c57a9b254f9b9307297640b211a
wcesarpsv <wcesarps@gmail.com>
2025-11-04 21:52:08 -0500

    Update Filter 3D 16th.py

--- pages/Filter 3D 16th.py
+++ pages/Filter 3D 16th.py
@@ -933,7 +933,7 @@ def treinar_modelo_3d_clusters_single(history, games_today):
         'Quadrant_Sin_XZ', 'Quadrant_Cos_XZ',
         'Quadrant_Sin_YZ', 'Quadrant_Cos_YZ',
         'Quadrant_Sin_Combo', 'Quadrant_Cos_Combo',
-        'Vector_Sign', 'Magnitude_3D','Asian_Line_Decimal'
+        'Vector_Sign', 'Magnitude_3D'
     ]
 
     extras_3d = history[features_3d].fillna(0)
